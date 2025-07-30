6ebf3d863c8a0ae4dfad6bccefa1a8e96a2496d4
emrahburak <gemrahburak@gmail.com>
2025-07-30 18:56:22 +0300

    UPDATED --config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -11,20 +12,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, // İstersen uyarı sınırını yükseltebilirsin
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react";
-            if (id.includes("fancybox")) return "vendor-fancybox";
-            return "vendor"; // Diğer tüm node_modules paketleri
-          }
-        },
-      },
     },
   },
 });
